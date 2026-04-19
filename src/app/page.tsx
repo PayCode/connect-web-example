4a0125a66c647de27603f0f50d5e246b2056d97d
Carlos Diez <cdiezmoran@gmail.com>
2026-04-19 01:03:45 -0600

    feat: simple tab change animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5"
             href="https://www.npmjs.com/package/@paycode/connect"
             target="_blank"
             rel="noopener noreferrer"
--- src/components/commands/index.tsx
+++ src/components/commands/index.tsx
@@ -1,4 +1,7 @@
 import type { PayCodeConnect } from "@paycode/connect";
+import { AnimatePresence, motion } from "motion/react";
+import { type PropsWithChildren, useState } from "react";
+import useMeasure from "react-use-measure";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Authenticate } from "./authenticate";
 import { Capabilities } from "./capabilities";
@@ -10,29 +13,74 @@ type CommandsSectionProps = {
   pc: PayCodeConnect | null;
 };
 
-export const CommandsSection = ({ pc }: CommandsSectionProps) => (
-  <Tabs defaultValue="emv" className="w-100">
-    <TabsList>
-      <TabsTrigger value="emv">EMV</TabsTrigger>
-      <TabsTrigger value="authenticate">Authenticate</TabsTrigger>
-      <TabsTrigger value="capabilities">Capabilities</TabsTrigger>
-      <TabsTrigger value="transaction">Transaction</TabsTrigger>
-      <TabsTrigger value="pin">PIN</TabsTrigger>
-    </TabsList>
-    <TabsContent value="emv">
-      <Emv pc={pc} />
-    </TabsContent>
-    <TabsContent value="authenticate">
-      <Authenticate pc={pc} />
-    </TabsContent>
-    <TabsContent value="capabilities">
-      <Capabilities pc={pc} />
-    </TabsContent>
-    <TabsContent value="transaction">
-      <Transaction pc={pc} />
-    </TabsContent>
-    <TabsContent value="pin">
-      <Pin pc={pc} />
-    </TabsContent>
-  </Tabs>
+export const CommandsSection = ({ pc }: CommandsSectionProps) => {
+  const [tab, setTab] = useState("emv");
+  const [elementRef, bounds] = useMeasure();
+
+  return (
+    <Tabs value={tab} onValueChange={setTab} className="w-100">
+      <TabsList>
+        <TabsTrigger value="emv">EMV</TabsTrigger>
+        <TabsTrigger value="authenticate">Authenticate</TabsTrigger>
+        <TabsTrigger value="capabilities">Capabilities</TabsTrigger>
+        <TabsTrigger value="transaction">Transaction</TabsTrigger>
+        <TabsTrigger value="pin">PIN</TabsTrigger>
+      </TabsList>
+      <AnimatePresence initial={false} mode="popLayout">
+        <motion.div
+          animate={{ height: bounds.height }}
+          transition={{ duration: 0.27, ease: [0.25, 1, 0.5, 1] }}
+        >
+          <div ref={elementRef}>
+            <TabsContent value="emv">
+              <AnimationWrapper animationKey="emv-section">
+                <Emv pc={pc} />
+              </AnimationWrapper>
+            </TabsContent>
+            <TabsContent value="authenticate">
+              <AnimationWrapper animationKey="auth-section">
+                <Authenticate pc={pc} />
+              </AnimationWrapper>
+            </TabsContent>
+            <TabsContent value="capabilities">
+              <AnimationWrapper animationKey="capabilities-section">
+                <Capabilities pc={pc} />
+              </AnimationWrapper>
+            </TabsContent>
+            <TabsContent value="transaction">
+              <AnimationWrapper animationKey="transaction-section">
+                <Transaction pc={pc} />
+              </AnimationWrapper>
+            </TabsContent>
+            <TabsContent value="pin">
+              <AnimationWrapper animationKey="pin-section">
+                <Pin pc={pc} />
+              </AnimationWrapper>
+            </TabsContent>
+          </div>
+        </motion.div>
+      </AnimatePresence>
+    </Tabs>
+  );
+};
+
+type AnimationWrapperProps = {
+  animationKey: string;
+};
+
+const AnimationWrapper = ({
+  animationKey,
+  children,
+}: PropsWithChildren<AnimationWrapperProps>) => (
+  <AnimatePresence propagate>
+    <motion.div
+      key={animationKey}
+      initial={{ opacity: 0, filter: "blur(4px)" }}
+      animate={{ opacity: 1, filter: "blur(0)" }}
+      exit={{ opacity: 0, filter: "blur(4px)" }}
+      transition={{ duration: 0.15, ease: [0.26, 0.08, 0.25, 1] }}
+    >
+      {children}
+    </motion.div>
+  </AnimatePresence>
 );
